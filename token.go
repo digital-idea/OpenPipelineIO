bae0f569ee3e869bf76cef3baf0b0d06b8913c54
hanwoong kim <hello@lazypic.org>
2019-07-14 23:09:19 +0900

    보충설명 추가함.

--- token.go
+++ token.go
@@ -17,7 +17,7 @@ func GetTokenFromHeader(r *http.Request) (string, error) {
 	return auth[1], nil
 }
 
-// TokenHandler 함수는 토큰으로 restAPI를 사용할 수 있는지 체크하는 함수이다.
+// TokenHandler 함수는 토큰으로 restAPI를 사용할 수 있는지 체크하고 아이디와 엑세스 레벨을 반환한다.
 func TokenHandler(r *http.Request, session *mgo.Session) (string, AccessLevel, error) {
 	if !*flagAuthmode {
 		return "unknown", 0, nil
