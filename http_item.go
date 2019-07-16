e2075f9cffeaeb8e0247ceb2c19527c1cf25f47b
kim han woong <khw7096@gmail.com>
2019-07-16 17:07:49 +0900

    빠진 설정을 추가함.

--- http_item.go
+++ http_item.go
@@ -1187,6 +1187,7 @@ func handleAddShotSubmit(w http.ResponseWriter, r *http.Request) {
 		i.ID = i.Name + "_" + i.Type
 		i.Seq = strings.Split(i.Name, "_")[0]
 		i.Status = ASSIGN
+		i.Comp.Status = ASSIGN
 		i.Thumpath = fmt.Sprintf("/%s/%s_%s.jpg", i.Project, i.Name, i.Type)
 		i.Platepath = fmt.Sprintf("/show/%s/seq/%s/%s/plate/", i.Project, i.Seq, i.Name)
 		i.Thummov = fmt.Sprintf("/show/%s/seq/%s/%s/plate/%s_%s.mov", i.Project, i.Seq, i.Name, i.Name, i.Type)
