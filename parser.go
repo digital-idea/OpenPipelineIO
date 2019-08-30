afc50f5737638d075f047426cf30a463fc04b1ae
kim han woong <khw7096@gmail.com>
2019-08-30 18:18:30 +0900

    status: 형태의 word 형태를 추가함.

--- parser.go
+++ parser.go
@@ -8,10 +8,11 @@ import (
 
 // Searchword 는 검색어를 종류별로 담는 자료구조이다.
 type Searchword struct {
-	words []string // 아래 항목에 들어가지 않는 나머지 단어의 집합
-	tasks []string // task:ani,comp
-	user  []string // @김한웅 형태로 시작하는 문자
-	tags  []string // #태그명 형태로 시작하는 문자
+	words  []string // 아래 항목에 들어가지 않는 나머지 단어의 집합
+	tasks  []string // task:ani,comp
+	user   []string // user:김한웅 형태로 시작하는 문자
+	tags   []string // tag:태그명 형태로 시작하는 문자
+	status []string // status: 형태로 시작하는 문자
 }
 
 // SearchwordParser 함수는 단어를 받아서 Searchword 자료구조로 반환한다.
@@ -38,14 +39,12 @@ func SearchwordParser(word string) Searchword {
 					break
 				}
 			}
-		case strings.HasPrefix(w, "@"): // '@'로 시작하는지 체크
-			searchword.user = append(searchword.user, strings.TrimLeft(w, "@"))
 		case strings.HasPrefix(w, "user:"): // 'user:'로 시작하는지 체크
 			searchword.user = append(searchword.user, strings.TrimLeft(w, "user:"))
-		case strings.HasPrefix(w, "#"): // '#'으로 시작하는지 체크
-			searchword.tags = append(searchword.tags, strings.TrimLeft(w, "#"))
-		case strings.HasPrefix(w, "tag:"): // 'tag:'으로 시작하는지 체크
+		case strings.HasPrefix(w, "tag:"): // 'tag:'로 시작하는지 체크
 			searchword.tags = append(searchword.tags, strings.TrimLeft(w, "tag:"))
+		case strings.HasPrefix(w, "status:"): // 'status:'로 시작하는지 체크
+			searchword.status = append(searchword.status, strings.TrimLeft(w, "status:"))
 		default:
 			//중복 검색어 체크
 			hasWord := false
