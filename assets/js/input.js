da8bedce7ac2b05eb4e4f3a9b298ee094e5d1436
kim han woong <khw7096@gmail.com>
2019-09-10 15:48:19 +0900

    tag 수정을 좀더 편리하게 변경함.

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,22 +1,4 @@
-function setTags(project, name, tags, token) {
-    $.ajax({
-        url: "/api/settags",
-        type: "post",
-        data: {
-            project: project,
-            name: name,
-            tags: tags,    
-        },
-        headers: {
-            "Authorization": "Basic "+ token
-        },
-        dataType: "json",
-        success: function(data) {
-            console.info(data)
-        }
 
-    });
-}
 
 function setAssignTask(project, name, task, status, token) {
     $.ajax({
