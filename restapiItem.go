6b926a507ad4b6b9806d05b56e02fb1ae17a7e5c
kim han woong <khw7096@gmail.com>
2020-04-21 16:24:30 +0900

    taskanduserstatusnum 추가함.

--- restapiItem.go
+++ restapiItem.go
@@ -6787,6 +6787,66 @@ func handleAPITaskStatusNum(w http.ResponseWriter, r *http.Request) {
 	w.Write(data)
 }
 
+// handleAPITaskAndUserStatusNum 함수는 project, task, user를 입력받아서 각 status의 갯수를 반환한다.
+func handleAPITaskAndUserStatusNum(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Post Only", http.StatusMethodNotAllowed)
+		return
+	}
+	type Recipe struct {
+		Project string `json:"project"`
+		Task    string `json:"task"`
+		User    string `json:"user"`
+		UserID  string `json:"userid"`
+		Infobarnum
+	}
+	rcp := Recipe{}
+	session, err := mgo.Dial(*flagDBIP)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer session.Close()
+	rcp.UserID, _, err = TokenHandler(r, session)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+	r.ParseForm()
+	project := r.FormValue("project")
+	if project == "" {
+		http.Error(w, "project를 설정해주세요", http.StatusBadRequest)
+		return
+	}
+	rcp.Project = project
+	task := r.FormValue("task")
+	if task == "" {
+		http.Error(w, "task를 설정해주세요", http.StatusBadRequest)
+		return
+	}
+	rcp.Task = task
+	user := r.FormValue("user")
+	if task == "" {
+		http.Error(w, "task를 설정해주세요", http.StatusBadRequest)
+		return
+	}
+	rcp.User = user
+	statusnum, err := TotalTaskAndUserStatusnum(session, project, task, user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	rcp.Infobarnum = statusnum
+	data, err := json.Marshal(rcp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
 // handleAPIStatusNum 함수는 project를 입력받아서 각 status의 갯수를 반환한다.
 func handleAPIStatusNum(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
