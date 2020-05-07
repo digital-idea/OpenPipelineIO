44529a2bbf87137e0b2d93f4fe03c4bda535cc3d
kim han woong <khw7096@gmail.com>
2020-05-07 16:56:11 +0900

    필요한 자료구조 추가함.

--- review.go
+++ review.go
@@ -3,6 +3,7 @@ package main
 // Review 는 리뷰데이터 자료구조 이다.
 type Review struct {
 	Project     string    `json:"project"`     // 프로젝트
+	Name        string    `json:"name"`        // 샷네임, 에셋네임
 	Task        string    `json:"task"`        // 태스크
 	Createtime  string    `json:"createtime"`  // 생성시간
 	Updatetime  string    `json:"updatetime"`  // 업데이트 시간
