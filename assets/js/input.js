cf7089d4de7dd5e8958a10d849c1d47fda4def6d
kim han woong <khw7096@gmail.com>
2020-06-30 14:43:49 +0900

    주석 수정

--- assets/js/input.js
+++ assets/js/input.js
@@ -3902,10 +3902,10 @@ function removeDrawing() {
 // copyButton 은 value 값을 받아서, 클립보드로 복사하는 기능이다.
 function copyButton(value) {
     let id = document.createElement("input");   // input요소를 만듬
-    id.setAttribute("value", value);        // input요소에 값을 추가
+    id.setAttribute("value", value);            // input요소에 값을 추가
     document.body.appendChild(id);              // body에 요소 추가
     id.select();                                // input요소를 선택
     document.execCommand("copy");               // 복사기능 실행
     document.body.removeChild(id);              // body에 요소 삭제
-    alert(value + "\n값이 클립보드에 복사되었습니다.")
+    alert(value + "\n값이 클립보드에 복사되었습니다.");
 }
