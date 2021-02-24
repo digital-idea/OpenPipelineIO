c48b3b1977cf700d00de772c3a4e1aa4c2845c89
woong <woong@lazypic.org>
2021-02-24 19:23:42 +0900

    자료구조와 웹 메뉴 생성.

--- stage.go
+++ stage.go
@@ -2,20 +2,19 @@ package main
 
 import "errors"
 
-// Status 자료구조는 상태 자료구조이다.
-type Status struct {
-	ID          string  `json:"id"`          // ID, 상태코드
+// Stage 자료구조는 Review Stage의 자료구조이다.
+type Stage struct {
+	ID          string  `json:"id"`          // ID, Stage 문자코드
 	TextColor   string  `json:"textcolor"`   // TEXT 색상
 	BGColor     string  `json:"bgcolor"`     // BG 상태 색상
 	BorderColor string  `json:"bordercolor"` // Border 색상
 	Description string  `json:"description"` // 설명
-	Order       float64 `json:"order"`       // Status 우선순위
-	DefaultOn   bool    `json:"defaulton"`   // 검색바 기본선택 여부
-	InitStatus  bool    `json:"initstatus"`  // 아이템 생성시 최초 설정되는 Status 설정값
+	Order       float64 `json:"order"`       // ReviewStage 우선순위
+	InitStatus  bool    `json:"initstatus"`  // 아이템 생성시 최초 설정되는 ReviewStage 값
 }
 
-// CheckError 메소드는 Status 자료구조의 에러를 체크한다.
-func (s *Status) CheckError() error {
+// CheckError 메소드는 Stage 자료구조의 에러를 체크한다.
+func (s *Stage) CheckError() error {
 	if s.ID == "" {
 		return errors.New("ID가 빈 문자열 입니다")
 	}
