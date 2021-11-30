669cda97aa7313cf259ec096da34e96cd5a6c918
Bailey <bsy2658@gmail.com>
2021-11-30 12:39:44 +0900

    REST API PUT 추사

--- documents/rest_partner.md
+++ documents/rest_partner.md
@@ -2,7 +2,11 @@
 Partner Restapi 입니다.
 
 ## Get
-
 | URI | Description | Attributes | Curl Example |
 | --- | --- | --- | --- |
 |/api/partner/|파트너 정보를 가져옵니다|id|curl -H "Authorization: Basic <TOKEN>“ -X GET http://csi.lazypic.org/api/partner?id={id}
+
+## POST
+| URI | Description | Attributes | Curl Example |
+| --- | --- | --- | --- |
+|/api/partner/|새로운 파트너 정보를 추가합니다|name, homepage, address, phone, email. timezone, descriptioon|curl -H "Authorization: Basic <TOKEN>“ -X POST -d "name=test" http://csi.lazypic.org/api/partner
--- restapiPartner.go
+++ restapiPartner.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -56,6 +57,81 @@ func handleAPIPartner(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
+		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write(data)
+		return
+	} else if r.Method == http.MethodPost {
+		// POST 메소드는 새로운 사용자 정보를 DB에 저장한다
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		//mongoDB client 연결
+		client, err := mongo.NewClient(options.Client().ApplyURI(*flagMongoDBURI))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		err = client.Connect(ctx)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer client.Disconnect(ctx)
+		err = client.Ping(ctx, readpref.Primary())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// token 체크
+		_, _, err = TokenHandlerV2(r, client)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// 파트너 생성
+		p := Partner{}
+		p.ID = primitive.NewObjectID()
+		// 파트너 정보 Parsing
+		err = r.ParseForm()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		name := r.FormValue("name")
+		if name == "" {
+			http.Error(w, "name을 설정해주세요", http.StatusBadRequest)
+			return
+		}
+		homepage := r.FormValue("homepage")
+		address := r.FormValue("address")
+		phone := r.FormValue("phone")
+		email := r.FormValue("email")
+		timezone := r.FormValue("timezone")
+		description := r.FormValue("description")
+		p.Name = name
+		p.Homepage = homepage
+		p.Address = address
+		p.Phone = phone
+		p.Email = email
+		p.Timezone = timezone
+		p.Description = description
+		// 파트너 추가
+		err = addPartner(client, p)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// Response
+		item, err := getPartner(client, p.ID.Hex())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		data, err := json.Marshal(item)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
