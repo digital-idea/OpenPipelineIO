18c92e4cbb086909b1432c3dadb3262f2d930781
woong <woong@lazypic.org>
2022-01-28 15:45:45 -0600

    status 옵션에서 status event 옵션을 설정할 수 있도록 함.

--- status.go
+++ status.go
@@ -4,14 +4,15 @@ import "errors"
 
 // Status 자료구조는 상태 자료구조이다.
 type Status struct {
-	ID          string  `json:"id"`          // ID, 상태코드
-	TextColor   string  `json:"textcolor"`   // TEXT 색상
-	BGColor     string  `json:"bgcolor"`     // BG 상태 색상
-	BorderColor string  `json:"bordercolor"` // Border 색상
-	Description string  `json:"description"` // 설명
-	Order       float64 `json:"order"`       // Status 우선순위
-	DefaultOn   bool    `json:"defaulton"`   // 검색바 기본선택 여부
-	InitStatus  bool    `json:"initstatus"`  // 아이템 생성시 최초 설정되는 Status 설정값
+	ID                string  `json:"id"`                // ID, 상태코드
+	TextColor         string  `json:"textcolor"`         // TEXT 색상
+	BGColor           string  `json:"bgcolor"`           // BG 상태 색상
+	BorderColor       string  `json:"bordercolor"`       // Border 색상
+	Description       string  `json:"description"`       // 설명
+	Order             float64 `json:"order"`             // Status 우선순위
+	DefaultOn         bool    `json:"defaulton"`         // 검색바 기본선택 여부
+	InitStatus        bool    `json:"initstatus"`        // 아이템 생성시 최초 설정되는 Status 설정값
+	ReviewStatusEvent string  `json:"reviewstatusevent"` // 리뷰상태 변경시 발생하는 이벤트
 }
 
 // CheckError 메소드는 Status 자료구조의 에러를 체크한다.
@@ -23,7 +24,7 @@ func (s *Status) CheckError() error {
 		return errors.New("status 이름은 영문 대,소문자 또는 숫자로만 이루어져야 합니다")
 	}
 	if !regexWebColor.MatchString(s.TextColor) {
-		return errors.New("Text 컬러가 웹컬러(#FFFFFF 형태) 문자열이 아닙니다")
+		return errors.New("text 컬러가 웹컬러(#FFFFFF 형태) 문자열이 아닙니다")
 	}
 	if !regexWebColor.MatchString(s.BGColor) {
 		return errors.New("BG 컬러가 웹컬러(#FFFFFF 형태) 문자열이 아닙니다")
