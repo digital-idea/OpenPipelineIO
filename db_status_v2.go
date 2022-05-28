c87787a45291ab6d4f25383e09cbd0b2caa651f1
woong <woong@lazypic.org>
2022-05-28 20:41:31 +0900

    샷 status v2 값이 연산 될 수 있도록 함.

---
+++ db_status_v2.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func allStatusV2(client *mongo.Client) ([]Status, error) {
+	collection := client.Database("setting").Collection("status")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	findOptions := options.Find()
+	findOptions.SetSort(bson.D{{"order", -1}})
+	var status []Status
+	cursor, err := collection.Find(ctx, bson.D{}, findOptions)
+	if err != nil {
+		return status, err
+	}
+	err = cursor.All(ctx, &status)
+	if err != nil {
+		return status, err
+	}
+	return status, nil
+}
