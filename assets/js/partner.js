9aa33b233d92d4314240f46a090c175395b5500a
hanwoong kim <hello@lazypic.org>
2022-06-10 17:58:23 +0900

    주석 수정함.

--- assets/js/partner.js
+++ assets/js/partner.js
@@ -1,102 +1,100 @@
-
-var struct = "partner"
-var prefix = struct + "-" // ux id prefix string
-var endpoint = "/api/" + struct // restAPI endpoint
-var page = struct + "s" // webpage
+var webpage = "/partners" // webpage
+var endpoint = "/api/partner" // restAPI Endpoint
+var uxprefix = "partner-" // UX prefix string
 
 function UxToObject(data) {
-    data.name = document.getElementById(prefix + 'name').value
-    data.symbol = document.getElementById(prefix + 'symbol').value
-    data.domain = document.getElementById(prefix + 'domain').value
-    data.size = document.getElementById(prefix + 'size').value
-    data.homepage = document.getElementById(prefix + 'homepage').value
-    data.address = document.getElementById(prefix + 'address').value
-    data.phone = document.getElementById(prefix + 'phone').value
-    data.email = document.getElementById(prefix + 'email').value
-    data.location = document.getElementById(prefix + 'location').value
-    data.timezone = document.getElementById(prefix + 'timezone').value
-    data.description = document.getElementById(prefix + 'description').value
-    data.businessregistrationnumber = document.getElementById(prefix + 'businessregistrationnumber').value
-    data.manager = document.getElementById(prefix + 'manager').value
-    data.managerphone = document.getElementById(prefix + 'managerphone').value
-    data.manageremail = document.getElementById(prefix + 'manageremail').value
-    data.ftp = document.getElementById(prefix + 'ftp').value
-    data.ftpid = document.getElementById(prefix + 'ftpid').value
-    data.ftppw = document.getElementById(prefix + 'ftppw').value
-    data.opentime = document.getElementById(prefix + "opentime").options[document.getElementById(prefix + "opentime").selectedIndex].value,    
-    data.closedtime = document.getElementById(prefix + "closedtime").options[document.getElementById(prefix + "closedtime").selectedIndex].value,
-    data.paymentdate = document.getElementById(prefix + 'paymentdate').value
-    data.bank = document.getElementById(prefix + 'bank').value
-    data.bankaccount = document.getElementById(prefix + 'bankaccount').value
-    data.monetaryunit = document.getElementById(prefix + "monetaryunit").options[document.getElementById(prefix + "monetaryunit").selectedIndex].value,
-    data.projecthistory = document.getElementById(prefix + 'projecthistory').value
-    data.reputation = document.getElementById(prefix + 'reputation').value
-    data.status = document.getElementById(prefix + 'status').value
-    data.codename = document.getElementById(prefix + 'codename').value
-    data.companytype = document.getElementById(prefix + "companytype").options[document.getElementById(prefix + "companytype").selectedIndex].value,
-    data.contactpoint = document.getElementById(prefix + 'contactpoint').value
-    data.pmsurl = document.getElementById(prefix + 'pmsurl').value
-    data.isabroad = document.getElementById(prefix + 'isabroad').checked
-    data.isclient = document.getElementById(prefix + 'isclient').checked
-    data.tags = string2array(document.getElementById(prefix + 'tags').value)
+    data.name = document.getElementById(uxprefix + 'name').value
+    data.symbol = document.getElementById(uxprefix + 'symbol').value
+    data.domain = document.getElementById(uxprefix + 'domain').value
+    data.size = document.getElementById(uxprefix + 'size').value
+    data.homepage = document.getElementById(uxprefix + 'homepage').value
+    data.address = document.getElementById(uxprefix + 'address').value
+    data.phone = document.getElementById(uxprefix + 'phone').value
+    data.email = document.getElementById(uxprefix + 'email').value
+    data.location = document.getElementById(uxprefix + 'location').value
+    data.timezone = document.getElementById(uxprefix + 'timezone').value
+    data.description = document.getElementById(uxprefix + 'description').value
+    data.businessregistrationnumber = document.getElementById(uxprefix + 'businessregistrationnumber').value
+    data.manager = document.getElementById(uxprefix + 'manager').value
+    data.managerphone = document.getElementById(uxprefix + 'managerphone').value
+    data.manageremail = document.getElementById(uxprefix + 'manageremail').value
+    data.ftp = document.getElementById(uxprefix + 'ftp').value
+    data.ftpid = document.getElementById(uxprefix + 'ftpid').value
+    data.ftppw = document.getElementById(uxprefix + 'ftppw').value
+    data.opentime = document.getElementById(uxprefix + "opentime").options[document.getElementById(uxprefix + "opentime").selectedIndex].value,    
+    data.closedtime = document.getElementById(uxprefix + "closedtime").options[document.getElementById(uxprefix + "closedtime").selectedIndex].value,
+    data.paymentdate = document.getElementById(uxprefix + 'paymentdate').value
+    data.bank = document.getElementById(uxprefix + 'bank').value
+    data.bankaccount = document.getElementById(uxprefix + 'bankaccount').value
+    data.monetaryunit = document.getElementById(uxprefix + "monetaryunit").options[document.getElementById(uxprefix + "monetaryunit").selectedIndex].value,
+    data.projecthistory = document.getElementById(uxprefix + 'projecthistory').value
+    data.reputation = document.getElementById(uxprefix + 'reputation').value
+    data.status = document.getElementById(uxprefix + 'status').value
+    data.codename = document.getElementById(uxprefix + 'codename').value
+    data.companytype = document.getElementById(uxprefix + "companytype").options[document.getElementById(uxprefix + "companytype").selectedIndex].value,
+    data.contactpoint = document.getElementById(uxprefix + 'contactpoint').value
+    data.pmsurl = document.getElementById(uxprefix + 'pmsurl').value
+    data.isabroad = document.getElementById(uxprefix + 'isabroad').checked
+    data.isclient = document.getElementById(uxprefix + 'isclient').checked
+    data.tags = string2array(document.getElementById(uxprefix + 'tags').value)
     return data
 }
 
 function ObjectToUx(data) {
-    document.getElementById(prefix + 'id').value = data.id
-    document.getElementById(prefix + 'name').value = data.name
-    document.getElementById(prefix + 'symbol').value = data.symbol
-    document.getElementById(prefix + 'domain').value = data.domain
-    document.getElementById(prefix + 'size').value = data.size
-    document.getElementById(prefix + 'homepage').value = data.homepage
-    document.getElementById(prefix + 'address').value = data.address
-    document.getElementById(prefix + 'phone').value = data.phone
-    document.getElementById(prefix + 'email').value = data.email
-    document.getElementById(prefix + 'location').value = data.location
-    document.getElementById(prefix + 'timezone').value = data.timezone
-    document.getElementById(prefix + 'description').value = data.description
-    document.getElementById(prefix + 'businessregistrationnumber').value = data.businessregistrationnumber
-    document.getElementById(prefix + 'manager').value = data.manager
-    document.getElementById(prefix + 'managerphone').value = data.managerphone
-    document.getElementById(prefix + 'manageremail').value = data.manageremail
-    document.getElementById(prefix + 'ftp').value = data.ftp
-    document.getElementById(prefix + 'ftpid').value = data.ftpid
-    document.getElementById(prefix + 'ftppw').value = data.ftppw
-    document.getElementById(prefix + "opentime").value = data.opentime
-    document.getElementById(prefix + "closedtime").value = data.closedtime
-    document.getElementById(prefix + 'paymentdate').value = data.paymentdate
-    document.getElementById(prefix + 'bank').value = data.bank
-    document.getElementById(prefix + 'bankaccount').value = data.bankaccount
-    document.getElementById(prefix + "monetaryunit").value = data.monetaryunit
-    document.getElementById(prefix + 'projecthistory').value = data.projecthistory
-    document.getElementById(prefix + 'reputation').value = data.reputation
-    document.getElementById(prefix + 'status').value = data.status
-    document.getElementById(prefix + 'codename').value = data.codename
-    document.getElementById(prefix + "companytype").value = data.companytype
-    document.getElementById(prefix + 'contactpoint').value = data.contactpoint
-    document.getElementById(prefix + 'pmsurl').value = data.pmsurl
-    document.getElementById(prefix + 'isabroad').checked = data.isabroad
-    document.getElementById(prefix + 'isclient').checked = data.isclient
-    document.getElementById(prefix + 'tags').value = data.tags.join(",")
+    document.getElementById(uxprefix + 'id').value = data.id
+    document.getElementById(uxprefix + 'name').value = data.name
+    document.getElementById(uxprefix + 'symbol').value = data.symbol
+    document.getElementById(uxprefix + 'domain').value = data.domain
+    document.getElementById(uxprefix + 'size').value = data.size
+    document.getElementById(uxprefix + 'homepage').value = data.homepage
+    document.getElementById(uxprefix + 'address').value = data.address
+    document.getElementById(uxprefix + 'phone').value = data.phone
+    document.getElementById(uxprefix + 'email').value = data.email
+    document.getElementById(uxprefix + 'location').value = data.location
+    document.getElementById(uxprefix + 'timezone').value = data.timezone
+    document.getElementById(uxprefix + 'description').value = data.description
+    document.getElementById(uxprefix + 'businessregistrationnumber').value = data.businessregistrationnumber
+    document.getElementById(uxprefix + 'manager').value = data.manager
+    document.getElementById(uxprefix + 'managerphone').value = data.managerphone
+    document.getElementById(uxprefix + 'manageremail').value = data.manageremail
+    document.getElementById(uxprefix + 'ftp').value = data.ftp
+    document.getElementById(uxprefix + 'ftpid').value = data.ftpid
+    document.getElementById(uxprefix + 'ftppw').value = data.ftppw
+    document.getElementById(uxprefix + "opentime").value = data.opentime
+    document.getElementById(uxprefix + "closedtime").value = data.closedtime
+    document.getElementById(uxprefix + 'paymentdate').value = data.paymentdate
+    document.getElementById(uxprefix + 'bank').value = data.bank
+    document.getElementById(uxprefix + 'bankaccount').value = data.bankaccount
+    document.getElementById(uxprefix + "monetaryunit").value = data.monetaryunit
+    document.getElementById(uxprefix + 'projecthistory').value = data.projecthistory
+    document.getElementById(uxprefix + 'reputation').value = data.reputation
+    document.getElementById(uxprefix + 'status').value = data.status
+    document.getElementById(uxprefix + 'codename').value = data.codename
+    document.getElementById(uxprefix + "companytype").value = data.companytype
+    document.getElementById(uxprefix + 'contactpoint').value = data.contactpoint
+    document.getElementById(uxprefix + 'pmsurl').value = data.pmsurl
+    document.getElementById(uxprefix + 'isabroad').checked = data.isabroad
+    document.getElementById(uxprefix + 'isclient').checked = data.isclient
+    document.getElementById(uxprefix + 'tags').value = data.tags.join(",")
 }
 
 function AddMode() {
-    document.getElementById(prefix + 'postbutton').hidden = false
-    document.getElementById(prefix + 'deletebutton').hidden = true
-    document.getElementById(prefix + 'putbutton').hidden = true
+    document.getElementById(uxprefix + 'postbutton').hidden = false
+    document.getElementById(uxprefix + 'deletebutton').hidden = true
+    document.getElementById(uxprefix + 'putbutton').hidden = true
     InitModal()
 }
 
 function EditMode() {
-    document.getElementById(prefix + 'postbutton').hidden = true
-    document.getElementById(prefix + 'deletebutton').hidden = true
-    document.getElementById(prefix + 'putbutton').hidden = false
+    document.getElementById(uxprefix + 'postbutton').hidden = true
+    document.getElementById(uxprefix + 'deletebutton').hidden = true
+    document.getElementById(uxprefix + 'putbutton').hidden = false
 }
 
 function DeleteMode() {
-    document.getElementById(prefix + 'postbutton').hidden = true
-    document.getElementById(prefix + 'deletebutton').hidden = false
-    document.getElementById(prefix + 'putbutton').hidden = true
+    document.getElementById(uxprefix + 'postbutton').hidden = true
+    document.getElementById(uxprefix + 'deletebutton').hidden = false
+    document.getElementById(uxprefix + 'putbutton').hidden = true
 }
 
 function string2array(str) {
@@ -112,8 +110,7 @@ function string2array(str) {
 }
 
 function InitModal() {
-    // id가 "partner-" 로 시작하는 input > text, input > checkbox, select를 초기화 한다.
-    let inputs = document.querySelectorAll("[id^='" + prefix + "']")
+    let inputs = document.querySelectorAll("[id^='" + uxprefix + "']")
     for (var i = 0; i < inputs.length; i += 1) {
         if (inputs[i].type === "checkbox") {
             inputs[i].checked = false
@@ -187,7 +184,7 @@ function Post() {
         tata.success('Add', data.name + "가 추가되었습니다.", {
             position: 'tr',
             duration: 5000,
-            onClick: goPage,
+            onClick: tataLink,
             onClose: null,
         })
     })
@@ -207,7 +204,7 @@ function Put() {
         })
         return
     }
-    fetch(endpoint+'/'+document.getElementById(prefix+'id').value, {
+    fetch(endpoint+'/'+document.getElementById(uxprefix+'id').value, {
         method: 'PUT',
         headers: {
             "Authorization": "Basic "+ document.getElementById("token").value,
@@ -231,7 +228,7 @@ function Put() {
         tata.success('Edit', data.name + "가 편집되었습니다.", {
             position: 'tr',
             duration: 5000,
-            onClick: goPage,
+            onClick: tataLink,
             onClose: null,
         })
     })
@@ -241,7 +238,7 @@ function Put() {
 }
 
 function Delete() {
-    fetch(endpoint+'/'+document.getElementById(prefix+'id').value, {
+    fetch(endpoint+'/'+document.getElementById(uxprefix+'id').value, {
         method: 'DELETE',
         headers: {
             "Authorization": "Basic "+ document.getElementById("token").value,
@@ -264,7 +261,7 @@ function Delete() {
         tata.success('Delete', data.name + "가 삭제되었습니다.", {
             position: 'tr',
             duration: 5000,
-            onClick: goPage,
+            onClick: tataLink,
             onClose: null,
         })
     })
@@ -273,6 +270,6 @@ function Delete() {
     });
 }
 
-function goPage() {
-    window.location.replace(page)
+function tataLink() {
+    window.location.replace(webpage)
 }
