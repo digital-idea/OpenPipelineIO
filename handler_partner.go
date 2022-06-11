4ea3491fc72f57e85cdc2f237b4ec4240becb0cc
hanwoong kim <hello@lazypic.org>
2022-06-11 10:11:35 +0900

    핸들러에 불필요한 자료구조 제거함.

--- handler_partner.go
+++ handler_partner.go
@@ -11,7 +11,6 @@ import (
 )
 
 func handlePartners(w http.ResponseWriter, r *http.Request) {
-	// 사용자 로그인 여부 확인
 	ssid, err := GetSessionID(r)
 	if err != nil {
 		http.Redirect(w, r, "/signin", http.StatusSeeOther)
@@ -21,7 +20,6 @@ func handlePartners(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/invalidaccess", http.StatusSeeOther)
 		return
 	}
-	//mongoDB client 연결
 	client, err := mongo.NewClient(options.Client().ApplyURI(*flagMongoDBURI))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -42,14 +40,10 @@ func handlePartners(w http.ResponseWriter, r *http.Request) {
 	}
 
 	type recipe struct {
-		User                 // 로그인한 사용자 정보
-		Partners   []Partner // 검색된 사용자를 담는 리스트
-		Tags       []string  // 부서,파트 태그
-		Searchword string    // searchform에 들어가는 문자
-		Partnernum int       // 검색된 파트너사수
-		Devmode    bool      // 개발모드
+		User
 		SearchOption
 		Setting          Setting
+		Partners         []Partner
 		Projectlist      []string
 		TasksettingNames []string
 		Stages           []Stage
