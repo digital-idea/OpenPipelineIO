1e5095f44a49ce37c3137abd4231c6c2e6acb7e3
hanwoong kim <hello@lazypic.org>
2022-06-18 13:19:03 +0900

    app 이름을 설정할 수 있도록 바꿈.

--- struct_setting.go
+++ struct_setting.go
@@ -3,6 +3,7 @@ package main
 // Setting 자료구조는 관리자 설정 자료구조이다.
 type Setting struct {
 	ID                             string `json:"id"`                             // 셋팅ID
+	AppName                        string `json:"appname"`                        // App 이름
 	RunScriptAfterSignup           string `json:"runscriptaftersignup"`           // 사용자 가입이후 실행될 쉘스크립트
 	RunScriptAfterEditUserProfile  string `json:"runscriptafteredituserprofile"`  // 사용자 정보 수정후 실행될 쉘스크립트
 	ExcludeProject                 string `json:"excludeproject"`                 // Search옵션에 제외할 프로젝트명, 마이그레이션 시 사용한다.
