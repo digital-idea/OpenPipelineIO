0b6444e556a6fb41228579d9bc8275d60313cd85
woong <woong@lazypic.org>
2022-06-20 17:55:36 +0900

    통계 부분을 추가함.

--- assets/js/statistics.js
+++ assets/js/statistics.js
@@ -108,3 +108,53 @@ function NeedDeadlinenum() {
     });
 }
 
+
+function TotalShotProgress() {
+    fetch("/api1/statistics/shot", {
+        method: 'GET',
+        headers: {"Authorization": "Basic "+ document.getElementById("token").value},
+    })
+    .then((response) => {
+        if (!response.ok) {
+            response.text().then(function (text) {
+                alert(text)
+                return
+            });
+        }
+        return response.json()
+    })
+    .then((obj) => {
+        let progress = document.getElementById("total-shot-progress")
+        let total = 0
+        for (let [key, value] of Object.entries(obj.total)) {
+            total += value
+        }
+        let order = ["assign","ready","wip","confirm","done","out","omit","hold","none"]
+        for (let i = 0; i < order.length; i++) { 
+            for (let [key, value] of Object.entries(obj.total)) {
+                if (value === 0) {
+                    continue
+                }
+                if (!(order[i] == key)) {
+                    continue
+                }
+                let opt = document.createElement('div');
+                opt.classList.add("progress-bar")
+                opt.classList.add("bg-"+key)
+                opt.role = "progressbar"
+                opt.style = "width: " + (value / total) * 100 + "%"
+                opt.setAttribute("aria-valuenow",value)
+                opt.setAttribute("aria-valuemin","0")
+                opt.setAttribute("aria-valuemax",total)
+                opt.setAttribute("data-bs-toggle","tooltip")
+                opt.setAttribute("data-bs-placement","top")
+                opt.setAttribute("title", key + ":" + value)
+                opt.innerHTML = key + "<br>" + value
+                progress.appendChild(opt);
+            }
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+    });
+}
