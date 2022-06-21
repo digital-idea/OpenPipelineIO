afdc6134acb005d8ad29548caaca0c00f248ef16
woong <woong@lazypic.org>
2022-06-21 12:02:33 +0900

    pipelinestep이 정렬될 수 있도록 처리함.

--- db_pipelinestep.go
+++ db_pipelinestep.go
@@ -8,6 +8,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func addPipelinestep(client *mongo.Client, s Pipelinestep) error {
@@ -83,7 +84,9 @@ func allPipelinesteps(client *mongo.Client) ([]Pipelinestep, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	var results []Pipelinestep
-	cursor, err := collection.Find(ctx, bson.D{})
+	opts := options.Find()
+	opts.SetSort(bson.M{"name": 1})
+	cursor, err := collection.Find(ctx, bson.D{}, opts)
 	if err != nil {
 		return results, err
 	}
