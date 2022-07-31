423f16dbde43fa919fba9ff447477c26e7434390
woong <woong@lazypic.org>
2022-07-31 20:43:39 +0800

    자본관련 RestAPi 추가

---
+++ db_money.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func addMoney(client *mongo.Client, s Money) error {
+	collection := client.Database(*flagDBName).Collection("money")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_, err := collection.InsertOne(ctx, s)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getMoney(client *mongo.Client, id string) (Money, error) {
+	collection := client.Database(*flagDBName).Collection("money")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	s := Money{}
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return s, err
+	}
+	err = collection.FindOne(ctx, bson.M{"_id": objID}).Decode(&s)
+	if err != nil {
+		return s, err
+	}
+	return s, nil
+}
+
+func rmMoney(client *mongo.Client, id string) error {
+	collection := client.Database(*flagDBName).Collection("money")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+	_, err = collection.DeleteOne(ctx, bson.M{"_id": objID})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func setMoney(client *mongo.Client, s Money) error {
+	collection := client.Database(*flagDBName).Collection("money")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_, err := collection.UpdateOne(
+		ctx,
+		bson.M{"_id": s.ID},
+		bson.D{{Key: "$set", Value: s}},
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
