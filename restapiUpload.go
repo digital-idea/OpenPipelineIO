86a2e804c37e80bc7b72022d1069083391dca334
hanwoong kim <hello@lazypic.org>
2022-08-14 00:59:02 +0900

    도움말을 영어로 설정함.

--- restapiUpload.go
+++ restapiUpload.go
@@ -18,16 +18,11 @@ import (
 
 // handleAPIUploadThumbnail 함수는 thumbnail 이미지를 업로드 하는 RestAPI 이다.
 func handleAPIUploadThumbnail(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Post Only", http.StatusMethodNotAllowed)
-		return
-	}
 	type Recipe struct {
 		Project string `json:"project"`
 		Name    string `json:"name"`
 		Type    string `json:"type"`
 		Path    string `json:"path"`
-		UserID  string `json:"userid"`
 	}
 	rcp := Recipe{}
 	session, err := mgo.Dial(*flagDBIP)
@@ -36,75 +31,75 @@ func handleAPIUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer session.Close()
-	rcp.UserID, _, err = TokenHandler(r, session)
+	_, _, err = TokenHandler(r, session)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusUnauthorized)
 		return
 	}
 	// 어드민 셋팅을 불러온다.
-	adminSetting, err := GetAdminSetting(session)
+	umask, err := strconv.Atoi(CachedAdminSetting.Umask)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	umask, err := strconv.Atoi(adminSetting.Umask)
+	uid, err := strconv.Atoi(CachedAdminSetting.ThumbnailImagePathUID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	uid, err := strconv.Atoi(adminSetting.ThumbnailImagePathUID)
+	gid, err := strconv.Atoi(CachedAdminSetting.ThumbnailImagePathGID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	gid, err := strconv.Atoi(adminSetting.ThumbnailImagePathGID)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	permission, err := strconv.ParseInt(adminSetting.ThumbnailImagePathPermission, 8, 64)
+	permission, err := strconv.ParseInt(CachedAdminSetting.ThumbnailImagePathPermission, 8, 64)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	// 폼을 분석한다.
-	err = r.ParseMultipartForm(int64(adminSetting.MultipartFormBufferSize))
+	err = r.ParseMultipartForm(int64(CachedAdminSetting.MultipartFormBufferSize))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	project := r.FormValue("project")
 	if project == "" {
-		http.Error(w, "project를 설정해주세요", http.StatusBadRequest)
+		http.Error(w, "need project", http.StatusBadRequest)
 		return
 	}
 	rcp.Project = project
 
 	name := r.FormValue("name")
 	if name == "" {
-		http.Error(w, "name을 설정해주세요", http.StatusBadRequest)
+		http.Error(w, "need name", http.StatusBadRequest)
 		return
 	}
 	rcp.Name = name
 	typ := r.FormValue("type")
 	if typ == "" {
-		http.Error(w, "type을 설정해주세요", http.StatusBadRequest)
-		return
+		itemType, err := Type(session, rcp.Project, rcp.Name)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		rcp.Type = itemType
+	} else {
+		rcp.Type = typ
 	}
-	rcp.Type = typ
 	if len(r.MultipartForm.File) == 0 { // 파일이 없다면 에러처리한다.
-		http.Error(w, "썸네일 이미지의 경로를 설정해주세요", http.StatusBadRequest)
+		http.Error(w, "need thumbnail image path", http.StatusBadRequest)
 		return
 	}
 	if len(r.MultipartForm.File) != 1 { // 파일이 복수일 때
-		http.Error(w, "썸네일 이미지가 여러개 설정되어있습니다", http.StatusBadRequest)
+		http.Error(w, "multiple files cannot be set", http.StatusBadRequest)
 		return
 	}
 	// 썸네일이 존재한다면 썸네일을 처리한다.
 	for _, files := range r.MultipartForm.File {
 		for _, f := range files {
 			if f.Size == 0 {
-				http.Error(w, "파일사이즈가 0 바이트입니다", http.StatusBadRequest)
+				http.Error(w, "file size is 0 bytes", http.StatusBadRequest)
 				return
 			}
 			file, err := f.Open()
@@ -123,12 +118,16 @@ func handleAPIUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 				}
 				// adminsetting에 설정된 썸네일 템플릿에 실제 값을 넣는다.
 				var thumbImgPath bytes.Buffer
-				thumbImgPathTmpl, err := template.New("thumbImgPath").Parse(adminSetting.ThumbnailImagePath)
+				thumbImgPathTmpl, err := template.New("thumbImgPath").Parse(CachedAdminSetting.ThumbnailImagePath)
 				if err != nil {
 					http.Error(w, err.Error(), http.StatusInternalServerError)
 					return
 				}
 				err = thumbImgPathTmpl.Execute(&thumbImgPath, rcp)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
 				// 썸네일 이미지가 이미 존재하는 경우 이미지 파일을 지운다.
 				if _, err := os.Stat(thumbImgPath.String()); os.IsExist(err) {
 					err = os.Remove(thumbImgPath.String())
@@ -161,7 +160,7 @@ func handleAPIUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 					http.Error(w, err.Error(), http.StatusInternalServerError)
 					return
 				}
-				resizedImage := imaging.Fill(img, adminSetting.ThumbnailImageWidth, adminSetting.ThumbnailImageHeight, imaging.Center, imaging.Lanczos)
+				resizedImage := imaging.Fill(img, CachedAdminSetting.ThumbnailImageWidth, CachedAdminSetting.ThumbnailImageHeight, imaging.Center, imaging.Lanczos)
 				err = imaging.Save(resizedImage, thumbImgPath.String())
 				if err != nil {
 					http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -169,7 +168,7 @@ func handleAPIUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 				}
 				rcp.Path = thumbImgPath.String()
 			default:
-				http.Error(w, "허용하지 않는 파일 포맷입니다", http.StatusBadRequest)
+				http.Error(w, "not support format", http.StatusBadRequest)
 				return
 			}
 		}
