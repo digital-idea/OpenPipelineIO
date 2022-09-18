e3fb3605c093eabc7b137576ec10b7a5aad8bb50
hanwoong kim <hello@lazypic.org>
2022-09-18 21:04:20 +0900

    필요한 자료구조 추가함.

--- struct_fullcalendar.go
+++ struct_fullcalendar.go
@@ -1,6 +1,7 @@
 package main
 
-// https://fullcalendar.io/docs/event-object
+//https://fullcalendar.io/docs/event-object
+// https://fullcalendar.io/docs/event-parsing
 type FullCalendarEvent struct {
 	ID               string            `json:"id"`               // ID
 	GroupID          string            `json:"groupId"`          // 그룹ID
@@ -18,23 +19,30 @@ type FullCalendarEvent struct {
 	DurationEditable bool              `json:"durationEditable"` // 기간수정 여부
 	ResourceEditable bool              `json:"resourceEditable"` // 리소스 수정가능 여부
 	Display          string            `json:"display"`          // 'auto','block','list-item','background','inverse-background','none' -> https://fullcalendar.io/docs/eventDisplay
+	Color            string            `json:"color"`            // Background + Border Color
 	BackgroundColor  string            `json:"backgroundColor"`  // 배경색
 	BorderColor      string            `json:"borderColor"`      // 테두리색
 	TextColor        string            `json:"textColor"`        // 글자색
 	ExtendedProps    map[string]string `json:"extendedProps"`    // 나머지 필요한 기능
+	ResourceId       string            `json:"resourceId"`       // Resource ID
+	ResourceIds      string            `json:"resorceIds"`       // Resource IDs
 	// Constraint // 일정을 설정할 때 제한을 둘때 사용한다.
 	// Overlap // false 또는 함수를 다룬다. Go 자료구조에서는 다루지 않는다.
 }
 
 // https://fullcalendar.io/docs/resource-object
+// https://fullcalendar.io/docs/resource-parsing
 type FullCalendarResource struct {
-	ID                   string            `json:"id"`                   // ID
-	Title                string            `json:"title"`                // 제목
-	ExtendedProps        map[string]string `json:"extendedProps"`        // 나머지 필요한 기능
-	EventBackgroundColor string            `json:"eventBackgroundColor"` // 배경색
-	EventBorderColor     string            `json:"eventBorderColor"`     // 테두리색
-	EventTextColor       string            `json:"eventTextColor"`       // 글자색
-	EventClassNames      []string          `json:"eventClassNames"`      // html에 렌더링할 때 attached 할 클레스 이름
+	ID                   string                 `json:"id"`                   // ID
+	Title                string                 `json:"title"`                // 제목
+	ExtendedProps        map[string]string      `json:"extendedProps"`        // 나머지 필요한 기능
+	EventColor           string                 `json:"eventColor"`           // 배경색 + 테두리색
+	EventBackgroundColor string                 `json:"eventBackgroundColor"` // 배경색
+	EventBorderColor     string                 `json:"eventBorderColor"`     // 테두리색
+	EventTextColor       string                 `json:"eventTextColor"`       // 글자색
+	EventClassNames      []string               `json:"eventClassNames"`      // html에 렌더링할 때 attached 할 클레스 이름
+	Children             []FullCalendarResource `json:"children"`             // 자식의 Resource IDs
+	ParentId             string                 `json:"parentId"`             // 부모의 Resource ID
 	// EventAllow
 	// EventConstraint // 일정을 설정할 때 제한을 둘때 사용한다.
 	// EventOverlap // false 또는 함수를 다룬다. Go 자료구조에서는 다루지 않는다.
