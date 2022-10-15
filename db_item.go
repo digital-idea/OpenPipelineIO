1f91d904e3912cb36b2ce4a0c27ce7534ac31049
hanwoong kim <hello@lazypic.org>
2022-10-15 19:59:43 +0900

    id가 중복되는지 체크하는 코드의 버그를 수정함.

--- db_item.go
+++ db_item.go
@@ -18,7 +18,7 @@ func addItemV2(client *mongo.Client, i Item) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	num, err := collection.CountDocuments(ctx, bson.M{"name": i.ID})
+	num, err := collection.CountDocuments(ctx, bson.M{"id": i.ID})
 	if err != nil {
 		return err
 	}
