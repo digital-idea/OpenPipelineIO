74f50340b3abf46085c3ba2cd86169a5ebf6b574
woong <woong@lazypic.org>
2022-11-27 19:25:41 +0900

    자료구조 추가함.

--- struct_item.go
+++ struct_item.go
@@ -159,7 +159,7 @@ type Publish struct {
 
 // Task 자료구조는 태크스 정보를 담는 자료구조이다.
 type Task struct {
-	Title        string               `json:"title"`        // 테스크 네임
+	Title        string               `json:"title"`        // 테스크 제목
 	UserID       string               `json:"userid"`       // 아티스트ID
 	User         string               `json:"user"`         // 아티스트명
 	UserComment  string               `json:"usercomment"`  // 아티스트 코멘트
@@ -167,8 +167,9 @@ type Task struct {
 	StatusV2     string               `json:"statusv2"`     // 샷 상태.
 	ReviewStatus string               `json:"reviewstatus"` // 리뷰상태
 	BeforeStatus string               `json:"beforestatus"` // 이전상태
-	Startdate    string               `json:"startdate"`    // 작업 시작일 RFC3339
+	Startdate    string               `json:"startdate"`    // 1차 시작일 RFC3339 <- 최초시작일(툴이 커지면서 이 부분은 지져분해졌다.)
 	Predate      string               `json:"predate"`      // 1차 마감일 RFC3339
+	Start2nd     string               `json:"start2nd"`     // 2차 시작일 RFC3339
 	Date         string               `json:"date"`         // 2차 마감일 RFC3339
 	Mov          string               `json:"mov"`          // mov 경로
 	Mdate        string               `json:"mdate"`        // mov 업데이트 날짜 RFC3339
